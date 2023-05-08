77a62505a117d17e4ba10ee795e207c7da2a0895
wangxingkang <wang_xingkang@qq.com>
2023-05-08 22:49:12 +0800

    chore: add checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { checker } from 'vite-plugin-checker';
 import { visualizer } from 'rollup-plugin-visualizer'
 
 const analyze = process.env.ANALYZE;
@@ -15,7 +16,15 @@ export default defineConfig({
     },
   },
   plugins: [
-    react(),
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    checker({
+      typescript: true
+    }),
     analyze && visualizer({
       gzipSize: true,
       brotliSize: true,

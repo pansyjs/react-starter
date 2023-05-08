5a0335471e8f5a85c6b1cf254689f8e8bc86921a
wangxingkang <wang_xingkang@qq.com>
2023-05-08 18:35:30 +0800

    chore: project init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
+})

904d49f9e05a1e6842b99cee4f3e33acfe6715bd
wangxingkang <wang_xingkang@qq.com>
2023-05-08 21:51:38 +0800

    chore: add analyze scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+const analyze = process.env.ANALYZE;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +16,12 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    analyze && visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      open: true,
+    }),
     tsconfigPaths(),
-  ],
+  ].filter(Boolean),
 })

554e4fa8be216c6a5f1e561f425feda8aa7783e7
wangxingkang <wangxingkang@users.noreply.github.com>
2023-05-16 14:29:46 +0000

    deploy: 3815d87f3f43d584d60f72b7fe9c96f17a632b28

---
+++ assets/js/index-b00f8e3b.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-5b83373c.js";const d=()=>r("div",{children:"dashboard"});export{d as default};
--- assets/js/index-bb3f6f07.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-23345fe7.js";const d=()=>r("div",{children:"dashboard"});export{d as default};

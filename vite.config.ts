2fdc30e8ab1be4eabc1e25bceddde5bc4642529c
wangxingkang <wang_xingkang@qq.com>
2024-02-21 14:44:25 +0800

    chore: uesd vite-plugin-react-virtualized

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,12 @@
-import { URL } from 'node:url';
-import { join } from 'node:path';
-import { createRequire } from 'node:module';
-import { readFileSync, writeFileSync } from 'node:fs';
-import { defineConfig, type PluginOption } from 'vite'
+import { defineConfig } from 'vite'
 import Unocss from 'unocss/vite'
+import { virtualized } from 'vite-plugin-react-virtualized';
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 const analyze = process.env.ANALYZE;
 const isProd = process.env.NODE_ENV === 'production';
-
-const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`
-function reactVirtualized(): PluginOption {
-  return {
-    name: 'flat:react-virtualized',
-    configResolved: async () => {
-      const require = createRequire(import.meta.url)
-      const reactVirtualizedPath = require.resolve('react-virtualized')
-      const { pathname: reactVirtualizedFilePath } = new URL(reactVirtualizedPath, import.meta.url)
-      const file = reactVirtualizedFilePath
-        .replace(
-          join('dist', 'commonjs', 'index.js'),
-          join('dist', 'es', 'WindowScroller', 'utils', 'onScroll.js'),
-        )
-      const code = readFileSync(file, 'utf-8')
-      const modified = code.replace(WRONG_CODE, '')
-      writeFileSync(file, modified)
-    },
-  }
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -51,7 +28,7 @@ export default defineConfig({
     }) as any,
     Unocss(),
     tsconfigPaths(),
-    reactVirtualized(),
+    virtualized(),
   ],
   esbuild: {
     drop: ["console", "debugger"],

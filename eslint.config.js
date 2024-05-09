cde013d3cfee8fc0212905e29f0cd26e3dabb66c
wangxingkang <wang_xingkang@qq.com>
2024-05-09 23:19:57 +0800

    chore: add test:e2e scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-import antfu from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config';
 
 export default antfu(
   {
     react: true,
+    unocss: true,
+    formatters: true,
     stylistic: {
-      semi: true
-    }
-  }
-)
+      semi: true,
+    },
+  },
+);

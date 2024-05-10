bd0629d4b14559fd09ccc3867af466515f64a6ad
wangxingkang <wang_xingkang@qq.com>
2024-05-10 10:36:03 +0800

    chore: 删除无用文件

--- src/pages/login/components/LoginPage/index.tsx
+++ src/pages/login/components/LoginPage/index.tsx
@@ -9,9 +9,7 @@ interface LoginPageProps {
 export const LoginPage = (props: LoginPageProps) => {
   return (
     <div className={classnames(styles.login, 'relative w-full h-full px-4')}>
-      登录页面
       {props.children}
-      <div className="i-carbon:accessibility-alt" />
     </div>
   )
 }

fdb45e8fe43c2ca0a6caf6c1ce1740b835dbd1ad
wangxingkang <wangxingkang@users.noreply.github.com>
2024-06-19 05:20:02 +0000

    deploy: f2c0a60a053b3abd9a23ea55cf6f5fbae2344a61

--- assets/js/index-6eJuhs3T.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-URZ_sx3w.js";const t=()=>r.jsx("div",{children:"dashboard"});export{t as default};
---
+++ assets/js/index-C1MI5GCM.js
@@ -0,0 +1,1 @@
+import{F as r}from"./index-BOh6x8-U.js";const s=()=>r.jsx("div",{children:"Dashboard Page"});export{s as default};
